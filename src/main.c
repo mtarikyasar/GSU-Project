a4d3c1eb39cc4f35682ed39c10bb1e12afa81602
mtarikyasar <47866841+mtarikyasar@users.noreply.github.com>
2019-05-16 17:38:17 +0300

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -2,12 +2,18 @@
 #include"models.h"
 
 int main(int argc,char * argv[]){
-  int cevap;
+  int cevap = 1;
   
   //TODO: Read database, read_house_data();
 
-  while(cevap!=0){
-    printf("Emlak Programina Hosgeldiniz!\n");
+  int houseCount = 100;
+  House *houses = malloc(101*sizeof(struct house));
+  
+
+  houseCount = read_house_data("data_test.csv", houses) + 1;
+
+  while(cevap != 0){
+    printf("Emlak Programina Hos geldiniz!\n");
     printf("Yapmak istediginiz islemi seciniz\n");
     printf("1 - Evleri listele\n");
     printf("2 - ID degeri verilen evi goster\n");
@@ -21,98 +27,52 @@ int main(int argc,char * argv[]){
     
     switch (cevap) {
       case 1: {
-        print_house();
+        print_house(houses, houseCount);
         break;
       }
 
       case 2: {
-        get_house_byid();
+        //print_house(get_house_byid(houses, houseCount), houseCount);
         break;
       }
 
       case 3: {
-        get_neighborhoods();
+        //get_neighborhoods();
         break;
       }
 
       case 4: {
-        mean_sale_prices();
+        //mean_sale_prices();
         break;
       }
 
       case 5: {
-        sort_houses();
+        //sort_houses();
         //TODO: Top 10 house
         break;
       }
 
       case 6: {
-        sort_houses();
+        /*sort_houses();
         //TODO: Create file, save the list
         //Tarik'a NOT: switch - case icerisinde degisken yaratmak
         //sikinti cikarabilir, calismas ise hallederiz sonra.
         FILE *flist;
-        flist = fopen("sorted_list.txt", "w+");
-        if (flist == NULL) {
-          printf("Failed creating file...\n");
+        flist = fopen("sorted_list.txt", "w+"); */
+        
           break;
         }
                 
-                 
-                      
-        break;
-      }
-
       case 7: {
         //TODO: Price guess
         break;
       }
       
       default: {
-        pritf("Please enter an acceptable value\n");
+        printf("Please enter an acceptable value\n");
         break;
       }
         
     }
-        
-    /*
-    if (cevap==1){
-      printf("Ev listesi \n");
-      // TODO
-      // Butun evleri listele
-      print_house
-     }
-    else if (cevap==2){
-      printf("ID degeri verilen ev \n");
-      // TODO
-      // print_house ile tek evi goster
-      // get_house_byid cagrisi yap
-     }
-     else if (cevap==3){
-      printf("Komsu evler \n");
-      // TODO
-      // get_neighborhoods 
-     }
-     else if(cevap==4){
-       printf("Ortalama satislar\n");
-       // TODO
-       // mean_sale_prices
-     }
-     else if (cevap==5){
-       printf("En yuksek fiyatli evler\n");
-       // TODO 
-     }
-     else if (cevap==6){
-       printf("Sirali evler\n");
-       // TODO 
-     }
-     else if(cevap==7){
-       printf("Fiyat tahmini yap\n");
-       // TODO
-     }
-    printf("Programdan cikiliyor\n");
-    */
   }
-
-  return 0; 
 }
